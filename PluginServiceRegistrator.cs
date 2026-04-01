d2dca40dd7fe8c445cdf6eff4f8f8dd6d020dd06
wemre3131 <saoeditoer@gmail.com>
2026-04-01 12:02:21 +0200

    Update PluginServiceRegistrator.cs

--- PluginServiceRegistrator.cs
+++ PluginServiceRegistrator.cs
@@ -4,13 +4,9 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Jellyfin.Plugin.KavasakiPresence;
 
-/// <summary>
-/// Registers plugin services.
-/// </summary>
 public class PluginServiceRegistrator : IPluginServiceRegistrator
 {
-    /// <inheritdoc />
-    public void RegisterServices(IServiceCollection serviceCollection, IServiceProvider applicationServiceProvider)
+    public void RegisterServices(IServiceCollection serviceCollection)
     {
         serviceCollection.AddSingleton<DiscordService>();
         serviceCollection.AddSingleton<ImdbService>();
