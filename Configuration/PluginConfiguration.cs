2c6d4e02b809bce1ad80d7ad17bc857ac7bade30
wemre3131 <saoeditoer@gmail.com>
2026-04-01 14:29:46 +0200

    Update PluginConfiguration.cs

--- Configuration/PluginConfiguration.cs
+++ Configuration/PluginConfiguration.cs
@@ -5,21 +5,4 @@ namespace Jellyfin.Plugin.KavasakiPresence.Configuration;
 public class PluginConfiguration : BasePluginConfiguration
 {
     public string DiscordToken { get; set; } = string.Empty;
-    public bool IsEnabled { get; set; } = true;
-    public bool ShowTitle { get; set; } = true;
-    public bool ShowMediaType { get; set; } = true;
-    public bool ShowImdbRating { get; set; } = true;
-    public bool ShowImdbButton { get; set; } = true;
-    public bool ShowTimestamp { get; set; } = true;
-    public bool ShowEpisodeInfo { get; set; } = true;
-    public bool ShowYear { get; set; } = true;
-    public string CustomStatusText { get; set; } = string.Empty;
-    public string DiscordAppClientId { get; set; } = "1234567890123456789";
-    public bool ShowJellyfinButton { get; set; } = false;
-    public string JellyfinPublicUrl { get; set; } = string.Empty;
-
-    public PluginConfiguration()
-    {
-        // Default constructor
-    }
 }
